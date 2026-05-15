effc0d84014cd39e407d4c802f463222882fda15
cmpark0403-hue <cmpark0403@ajou.ac.kr>
2026-05-15 22:47:44 +0900

    찬스카드 생성 준비

--- src/main/java/com/example/mse/dto/ChanceCard.java
+++ src/main/java/com/example/mse/dto/ChanceCard.java
@@ -1,5 +1,8 @@
 package com.example.mse.dto;
 
 public enum ChanceCard {
-
+    EXTRA_THROW,      // 윷 한 번 더 던지기
+    MOVE_PLUS_ONE,   // 이동 칸 수 +1
+    SHIELD,          // 패널티 1회 방어
+    FORCE_BACK       // 상대 말 하나 뒤로 보내기
 }
